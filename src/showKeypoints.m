d6a1a6b4e0a3910ba42fc256ae276e103d2b755d
seba-hauns <seba.kirchner@gmail.com>
2014-11-03 16:11:57 +0100

    shows keypoints (no outofbounds check)

--- src/showKeypoints.m
+++ src/showKeypoints.m
@@ -1,9 +1,16 @@
-function [] = showKeypoints( image, keypoints )
+function [] = showpoint( image, keypoints )
 % Author: Sebastian Kirchner
 % input: image ... image (RGB double)
-%        keypoints ... keypoints image
+%        keypoints ... keypoints Vector
 
-imshow();
+imcopy = im2double(image);
+
+for i = 1:size(keypoints, 1)
+    m = keypoints(i,:);
+    imcopy= drawBorders(imcopy, m);
+end
+
+imshow(imcopy);
 
 
 end
@@ -17,6 +24,32 @@ function [] = showMatches( image1, image2, keypoints1, keypoints2 )
 
 imshow();
 
+
+end
+
+function [image_out] = drawBorders(image_in, point)
+
+imcopy = image_in;
+width = 3;
+length = 24;
+
+imcopy(point(1)-12:point(1)-10, point(2)-12:point(2)+12, 1) = 0;
+imcopy(point(1)-12:point(1)-10, point(2)-12:point(2)+12, 2) = 0;
+imcopy(point(1)-12:point(1)-10, point(2)-12:point(2)+12, 3) = 1;
+
+imcopy(point(1)+10:point(1)+12, point(2)-12:point(2)+12, 1) = 0;
+imcopy(point(1)+10:point(1)+12, point(2)-12:point(2)+12, 2) = 0;
+imcopy(point(1)+10:point(1)+12, point(2)-12:point(2)+12, 3) = 1;
+
+imcopy(point(1)-12:point(1)+12, point(2)-12:point(2)-10, 1) = 0;
+imcopy(point(1)-12:point(1)+12, point(2)-12:point(2)-10, 2) = 0;
+imcopy(point(1)-12:point(1)+12, point(2)-12:point(2)-10, 3) = 1;
+
+imcopy(point(1)-12:point(1)+12, point(2)+10:point(2)+12, 1) = 0;
+imcopy(point(1)-12:point(1)+12, point(2)+10:point(2)+12, 2) = 0;
+imcopy(point(1)-12:point(1)+12, point(2)+10:point(2)+12, 3) = 1;
+
+image_out = imcopy;
 
 end
 
