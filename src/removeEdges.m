1e2c8ffea45c86e277cfdf4d80a08350808a9631
seba-hauns <seba.kirchner@gmail.com>
2014-11-24 16:42:57 +0100

    debugged version of removeEdges, working testrun vs.

--- src/removeEdges.m
+++ src/removeEdges.m
@@ -4,14 +4,18 @@ function [ keypoints ] = removeEdges( extrema, im )
 % input: im ... double image (grayscale) of keypoint scale
 % output: keypoints ... extrema without edges (Harris-Corner Detector)
 
-
 tempkeys = zeros(size(extrema));
-count = 1;
-corners = zeros(size(im), max(extrema(:,3)));
+count = 0;
 
 % get corners for every scaling level of the image
 for j=1:max(extrema(:,3))
-    corners(:,:,j) = corner(im(:,:,j), Harris);
+    temp = corner(im(:,:,j), 'Harris');
+    
+    % corner returns X Y coordinates, but we need Y X coordinates, 
+    % ==> switch them
+    corners(:,:,j) = zeros(size(temp));
+    corners(:,2,j) = temp(:,1);
+    corners(:,1,j) = temp(:,2);
 end
 
 % loop through all keypoints
@@ -19,9 +23,10 @@ for i=1:size(extrema,1)
     
     % if the keypoint is in the keypoints of the found corners, add to
     % temporary keypoints
-    if ismember(extrema(i,2), corners(:,:,extrema(i,3)));
-        tempkeys(count,:) = extrema(i,:);
+    if ismember(extrema(i,1:2), corners(:,:,extrema(i,3)));
         count = count + 1;
+        tempkeys(count,:) = extrema(i,:);
+        
     end
 end
 
