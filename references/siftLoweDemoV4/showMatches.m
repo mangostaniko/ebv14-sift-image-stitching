cb2ab4ec287e640c17ca2580e954c70a40debbb6
HannaHuber <hanna_huber@hotmail.com>
2014-12-17 17:06:43 +0100

    commit match test

--- references/siftLoweDemoV4/showMatches.m
+++ references/siftLoweDemoV4/showMatches.m
@@ -24,13 +24,13 @@ jointImg = [imcopy1, imcopy2];
 
 % Add the width of the 1st image to the x-coordinates of keypoints of
 % 2nd image
-keypoints2 = [keypoints2(:,1)+size(imcopy1, 2) keypoints2(:,2)];
+keypoints2 = [keypoints2(:,2)+size(imcopy1, 2) keypoints2(:,1)];
 
 % Extract x and y Coordinates from keypoints1 and keypoints2 respectively
 % and add them two corresponding vector (for plotting the line between
 % the keypoints)
-kpXcoords = [keypoints1(:,1)+radius keypoints2(:,1)-radius];
-kpYcoords = [keypoints1(:,2) keypoints2(:,2)];
+kpXcoords = [keypoints1(:,2)+radius keypoints2(:,2)-radius];
+kpYcoords = [keypoints1(:,1) keypoints2(:,1)];
 
 figure;
 % plot the joint Image
@@ -41,27 +41,28 @@ for i = 1:size(keypoints1, 1)
     key1 = keypoints1(i,:);
     key2 = keypoints2(i,:);
     
-%     if size(keypoints, 1) == max(size(gradients))
-%         % calculates X and Y for the line points to draw the angle of the gradients
-%         deg_X1 = [key1(1), key1(1)+radius*cos(gradients1(i))];
-%         deg_Y1 = [key1(2), key1(2)-radius*sin(gradients1(i))];
-%         
-%         deg_X2 = [key2(1), key2(1)+radius*cos(gradients1(i))];
-%         deg_Y2 = [key2(2), key2(2)-radius*sin(gradients1(i))];
-%         
-%         % plot the lines for the angle of the gradients
-%         line(deg_X1, deg_Y1, 'Color', [1, 0, 0], 'LineWidth', lineWid);
-%         line(deg_X2, deg_Y2, 'Color', [1, 0, 0], 'LineWidth', lineWid);
-%     end
+    %     % calculates X and Y for the line points to draw the angle of the gradients
+    %     deg_X1 = [key1(1), key1(1)+radius*cos(gradients1(i))];
+    %     deg_Y1 = [key1(2), key1(2)-radius*sin(gradients1(i))];
+    %
+    %     deg_X2 = [key2(1), key2(1)+radius*cos(gradients1(i))];
+    %     deg_Y2 = [key2(2), key2(2)-radius*sin(gradients1(i))];
+    %
+    %     % plot the lines for the angle of the gradients
+    %     line(deg_X1, deg_Y1, 'Color', [1, 0, 0], 'LineWidth', lineWid);
+    %     line(deg_X2, deg_Y2, 'Color', [1, 0, 0], 'LineWidth', lineWid);
+    
     
     % plot the borders around each keypoint
     
-    rectangle('Curvature', [1 1], 'Position', [(key1(2)-radius) (key1(1)-radius) (2*radius) (2*radius)], 'EdgeColor', [0,0,1], 'LineWidth', lineWid);
-    rectangle('Curvature', [1 1], 'Position', [(key2(2)-radius) (key2(1)-radius) (2*radius) (2*radius)], 'EdgeColor', [0,0,1], 'LineWidth', lineWid);
-    
-    % plot the lines connecting the corresponding keypoints in Image1 and
-    % Image2
-    plot(kpXcoords(i,:), kpYcoords(i,:), 'Color', [1, 0, 0], 'LineWidth', 1.5);
+    if (sum(key1) == 0 || sum(key2) == 0)
+        rectangle('Curvature', [1 1], 'Position', [(key1(2)-radius) (key1(1)-radius) (2*radius) (2*radius)], 'EdgeColor', [0,0,1], 'LineWidth', lineWid);
+        rectangle('Curvature', [1 1], 'Position', [(key2(2)-radius) (key2(1)-radius) (2*radius) (2*radius)], 'EdgeColor', [0,0,1], 'LineWidth', lineWid);
+        
+        % plot the lines connecting the corresponding keypoints in Image1 and
+        % Image2
+        plot(kpXcoords(i,:), kpYcoords(i,:), 'Color', [1, 0, 0], 'LineWidth', 1.5);
+    end
 end
 
 % splits image1 and image2
--- references/siftLoweDemoV4/testMatchingWithLowe.m
+++ references/siftLoweDemoV4/testMatchingWithLowe.m
@@ -3,5 +3,5 @@
 [imA,desA,locA] = sift('B1.pgm');
 [imB,desB,locB] = sift('B2.pgm');
 
-matches = matchKeypoints(locA(:,1:2),locB(:,3:4),desA,desB)
+matches = matchKeypoints(locA(:,1:2),locB(:,1:2),desA,desB);
 showMatches(imA,imB,matches(:,1:2),matches(:,3:4));
