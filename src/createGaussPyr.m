083d6684fd10c254254db2f1ff76e9e45bfd6099
seba-hauns <seba.kirchner@gmail.com>
2015-01-05 12:05:44 +0100

    gaussPyr uses 2 filters and sigma is settable, createLoG uses createGaussPyr

--- src/createGaussPyr.m
+++ src/createGaussPyr.m
@@ -5,19 +5,27 @@ if (size(image, 3) == 3)
     image = rgb2gray(input_image);
 end
 
-gauss_filt = fspecial('gaussian', [5 5], 0.5);
+%% SET SIGMA HERE
+sigma = 0.5;
+
+%% USE 2 FILTERS FOR SPEED
+gauss_filt_1 = fspecial('gaussian', [1 5], sigma);
+gauss_filt_2 = fspecial('gaussian', [5 1], sigma);
 
 
 %% CREATE GAUSSIAN PYRAMID ==> BLUR then SCALE DOWN
 gaussian_pyramid(1).scale = image;
 
-filtered = imfilter(gaussian_pyramid(1).scale, gauss_filt, 'replicate');
+filtered = imfilter(gaussian_pyramid(1).scale, gauss_filt_1, 'replicate');
+filtered = imfilter(filtered, gauss_filt_2, 'replicate');
 gaussian_pyramid(2).scale = filtered(1:2:end,1:2:end);
 
-filtered = imfilter(gaussian_pyramid(2).scale, gauss_filt, 'replicate');
+filtered = imfilter(gaussian_pyramid(2).scale, gauss_filt_1, 'replicate');
+filtered = imfilter(filtered, gauss_filt_2, 'replicate');
 gaussian_pyramid(3).scale = filtered(1:2:end,1:2:end);
 
-filtered = imfilter(gaussian_pyramid(3).scale, gauss_filt, 'replicate');
+filtered = imfilter(gaussian_pyramid(3).scale, gauss_filt_1, 'replicate');
+filtered = imfilter(filtered, gauss_filt_2, 'replicate');
 gaussian_pyramid(4).scale = filtered(1:2:end,1:2:end);
 
 end
--- src/createLoG.m
+++ src/createLoG.m
@@ -9,17 +9,7 @@ gauss_filt = fspecial('gaussian', [5 5], 0.5);
 
 
 %% CREATE GAUSSIAN PYRAMID ==> BLUR then SCALE DOWN
-gaussian_pyramid(1).scale = image;
-
-filtered = imfilter(gaussian_pyramid(1).scale, gauss_filt, 'replicate');
-gaussian_pyramid(2).scale = filtered(1:2:end,1:2:end);
-
-filtered = imfilter(gaussian_pyramid(2).scale, gauss_filt, 'replicate');
-gaussian_pyramid(3).scale = filtered(1:2:end,1:2:end);
-
-filtered = imfilter(gaussian_pyramid(3).scale, gauss_filt, 'replicate');
-gaussian_pyramid(4).scale = filtered(1:2:end,1:2:end);
-
+gaussian_pyramid = createGaussPyr(image);
 
 %% CREATE LoGs
 size_image = size(gaussian_pyramid(1).scale);
